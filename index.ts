d7d133769e052f08c94ef7a326d6f678a0d1f988
lamh85 <lamh85@gmail.com>
2024-01-02 17:23:01 -0800

    WIP comma strategy. Year resolver.

--- index.ts
+++ index.ts
@@ -1,7 +1,67 @@
-// september 1, 2020
+type InputT = string
 
-const run = (input: string) => {
-  const inputSplit = input.split(/[^0-9a-zA-Z]/g)
+type NormalizedT = {
+  month: number | null
+  year: number | null
+  day: number | null
+  status: 'ok' | 'error'
+}
+
+const ERROR_RESULT: NormalizedT = {
+  month: null,
+  year: null,
+  day: null,
+  status: 'error',
+}
+
+const run = (input: InputT): NormalizedT => {
+  // First, do the obvious clues:
+  // month name, numbers with 4 digits, commas
+
+  // EG: september 1, 2020
+  if (input.includes(',')) {
+    return commaSolution()
+  }
+
+  // const inputSplit = input.split(/[^0-9a-zA-Z]/g)
+}
+
+// Only accepted formats:
+// 1 September, 2020
+// September 1, 2020
+const commaSolution = (input: InputT): NormalizedT => {
+  if (input.match(/,/g)?.length !== 1) {
+    return ERROR_RESULT
+  }
+
+  const commaSplit: string[] = input.split(',')
+  const possibleYear = commaSplit[1]
+
+  const year = resolveYear(possibleYear)
+  if (!year) {
+    return ERROR_RESULT
+  }
+}
+
+const resolveYear = (input: any): number | null => {
+  const NOT_FOUND = null
+
+  if (typeof Number(input) !== 'number') {
+    return NOT_FOUND
+  }
+
+  if (input.length === 4) {
+    return Number(input)
+  }
+
+  if (input.length !== 2) {
+    return NOT_FOUND
+  }
+
+  const currentYear = new Date().toISOString().split('-')[0]
+  const yearPadding = currentYear.slice(currentYear.length - 2)
+  const concatenated = Number(`${yearPadding}${String(input)}`)
+  return concatenated
 }
 
 const findMonthByName = (inputSplit: (string | number)[]) => {
